594544f47aa4295cb7bf8fef2c4ae97b4545c964
Quellen-Dojo <marquelle.nesbitt@gmail.com>
2022-01-26 23:10:40 -0800

    update styles and add txn sending

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { WalletProvider, ConnectionProvider } from '@solana/wallet-adapter-react';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { PhantomWalletAdapter,SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { PhantomWalletAdapter,SolflareWalletAdapter, SolletExtensionWalletAdapter, SolflareWebWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { WalletModalProvider, WalletDisconnectButton, WalletMultiButton, WalletConnectButton } from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
 import { useMemo } from 'react';
@@ -9,9 +9,10 @@ import Home from './Home';
 require('./solstyles.css');
 
 export default function App() {
-    const network = WalletAdapterNetwork.Devnet;
-    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-    const wallets = useMemo(() => [new PhantomWalletAdapter(), new SolflareWalletAdapter()], [network]);
+    const network = 'https://api.devnet.solana.com';
+    // const network = 'https://rough-green-pond.solana-mainnet.quiknode.pro/5a1a239b2dfa014a7882c9b902f94494676096cc/';
+    const endpoint = useMemo(() => network, [network]);
+    const wallets = useMemo(() => [new PhantomWalletAdapter(), new SolflareWalletAdapter(),new SolflareWebWalletAdapter(), new SolletExtensionWalletAdapter()], [network]);
     return (
         <ConnectionProvider endpoint={endpoint}>
             <WalletProvider wallets={wallets}>
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,41 +1,136 @@
 import { WalletDisconnectButton, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-import { useWallet, useConnection, useAnchorWallet } from '@solana/wallet-adapter-react';
-import { SystemProgram, Transaction, PublicKey, LAMPORTS_PER_SOL } from '@solana/web3.js';
-import { useCallback } from 'react';
+import { useWallet, useConnection, useAnchorWallet, AnchorWallet } from '@solana/wallet-adapter-react';
+import { SystemProgram, Transaction, PublicKey, LAMPORTS_PER_SOL, sendAndConfirmRawTransaction } from '@solana/web3.js';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import './carwash.css';
+
+type NFTMeta = {
+    name: string;
+    symbol: string;
+    description: string;
+    image: string;
+    attributes: { trait_type: string; value: string }[];
+    mint: string;
+};
 
 export default function Home() {
     const { publicKey, sendTransaction, wallet } = useWallet();
     const anchor = useAnchorWallet();
     const { connection } = useConnection();
     console.log(anchor);
+    console.log(publicKey);
 
-    const buttonOnClick = useCallback(async () => {
-        const txn = new Transaction().add(
-            SystemProgram.transfer({
-                fromPubkey: publicKey,
-                toPubkey: new PublicKey('3uAvEjbkSY7GL2vddbczYwJxXWu74HHrkZeFB96u6Bi5'),
-                lamports: 1 * LAMPORTS_PER_SOL,
-            })
-        );
+    const [highProcessing, setHighProcessing] = useState(false);
+
+    const sendTxn = async (to: string) => {
         try {
+            const txn = new Transaction().add(
+                SystemProgram.transfer({
+                    fromPubkey: anchor.publicKey,
+                    lamports: 0.001 * LAMPORTS_PER_SOL,
+                    toPubkey: new PublicKey(to),
+                })
+            );
             const sig = await sendTransaction(txn, connection);
-            await connection.confirmTransaction(sig, 'confirmed');
-        } catch (e) {
-            console.log(e);
+            console.log(`Signature: ${sig}`);
+            return sig;
+        } catch {
+            console.log('Rejected or TXN failed');
         }
-    }, [publicKey, sendTransaction, connection]);
+    };
 
     return (
         <div>
-            {!anchor && <WalletMultiButton />}
+            {!anchor && (
+                <nav>
+                    <WalletMultiButton />
+                </nav>
+            )}
             {anchor && (
                 <div>
-                    <WalletDisconnectButton />
-                    <button onClick={buttonOnClick}>Send 69 Lamports</button>
+                    <nav>
+                        <WalletDisconnectButton />
+                    </nav>
+                    <div className='container'>
+                        <h1>Select a BitWhip to wash!</h1>
+                    </div>
+                    <NFTDisplay
+                        payForWash={() => sendTxn('3uAvEjbkSY7GL2vddbczYwJxXWu74HHrkZeFB96u6Bi5')}
+                        wallet={anchor}
+                        processing={[highProcessing, setHighProcessing]}
+                    />
                 </div>
             )}
         </div>
     );
 }
 
-function TxnSender() {}
+function NFTDisplay(props: {
+    wallet: AnchorWallet;
+    payForWash: Function;
+    processing: [boolean, React.Dispatch<React.SetStateAction<boolean>>];
+}) {
+    
+
+    const filterNonCleaned = (metadataArray: Array<NFTMeta>) => {
+        const cleanedInAttributes = (attrs: { trait_type: string; value: string }[]) => {
+            for (const attr of attrs) {
+                if (attr.trait_type === 'Washed' && attr.value === 'Yes') {
+                    return true;
+                }
+            }
+            return false;
+        };
+
+        return metadataArray.filter(v => !cleanedInAttributes(v.attributes));
+    };
+
+    const filterLandevos = (metadataArray: Array<NFTMeta>) => {
+        return metadataArray.filter(v => v.description === 'BitWhips Series 1 - Landevo');
+    };
+
+    const [nftData, setNFTData] = useState([]);
+
+    
+
+    useEffect(() => {
+        const fetchMetadata = async () => {
+            const metadata = await (
+                await fetch(
+                    `https://bitwhipsmintback.herokuapp.com/getallwhips?wallet=${props.wallet.publicKey.toBase58()}&includeTopLevel=true`,
+                    {
+                        method: 'GET',
+                        headers: { 'Content-Type': 'application/json' },
+                    }
+                )
+            ).json();
+            setNFTData(metadata);
+        };
+        fetchMetadata();
+    }, []);
+
+    return (
+        <div className='container'>
+            <div className='nftContainer'>
+                {filterNonCleaned(filterLandevos(nftData)).map((v, k) => <NFTImage payForWash={props.payForWash} wallet={props.wallet} nftMetadata={v} key={k} />)}
+            </div>
+        </div>
+    );
+}
+
+function NFTImage(props: { nftMetadata: NFTMeta, payForWash: Function, wallet: AnchorWallet }) {
+    const submitNFTToWash = useCallback(
+        async () => {
+            const sig = await props.payForWash();
+            if (sig) {
+                const processRes = await fetch('http://localhost:3002/processcarwash', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ signature: sig, nft: props.nftMetadata, fromWallet: props.wallet.publicKey.toBase58() }),
+                });
+            }
+        },
+        []
+    );
+    return (<img className='nftImage' src={props.nftMetadata.image} onClick={submitNFTToWash} />);
+}
